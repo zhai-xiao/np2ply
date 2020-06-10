1e192186a1fbe1a68089be0b390aac3a72ab2211
Xiao <zhaixiao43+git@gmail.com>
2020-06-11 09:06:13 +1200

    add alpha, id and piece channel for better handling, remove color_uchar

--- usage.py
+++ usage.py
@@ -4,23 +4,26 @@ import numpy as np
 from np2ply import PLYWriter
 
 # set up the numpy arrays to output
+num_vert = 20
+num_face = 5
 # vertex positions
-x = np.random.rand(20)
-y = np.random.rand(20)
-z = np.random.rand(20)
+x = np.random.rand(num_vert)
+y = np.random.rand(num_vert)
+z = np.random.rand(num_vert)
 # vertex colors
-r = 255*np.random.rand(20)
-g = 255*np.random.rand(20)
-b = 255*np.random.rand(20)
+r = np.random.rand(num_vert)
+g = np.random.rand(num_vert)
+b = np.random.rand(num_vert)
+a = np.random.rand(num_vert)
 # vertex data channels
-vdata1 = np.random.rand(20)
-vdata2 = 2147483647*np.random.rand(20, 1) # a vertex int channel. There is no uint32 in houdini vex, so we can't go beyond int32
+vdata1 = np.random.rand(num_vert)
+vdata2 = 2147483647*np.random.rand(num_vert, 1) # a vertex int channel. There is no uint32 in houdini vex, so we can't go beyond int32
 # vertex normal
-vdata3 = np.random.rand(3,20)
+vdata3 = np.random.rand(3,num_vert)
 # face vertex indices
-findices = (20*np.random.rand(5, 4)).astype(int)
+findices = (num_vert*np.random.rand(num_face, 4)).astype(int)
 # face channel
-fdata = np.random.rand(5)
+fdata = np.random.rand(num_face)
 
 # set up the PLYWriter
 writer = PLYWriter(num_vertices=20, num_faces=5, face_type="quad")
@@ -28,10 +31,14 @@ writer = PLYWriter(num_vertices=20, num_faces=5, face_type="quad")
 writer.add_vertex_pos(x,y,z)
 writer.add_faces(findices)
 # fill optional channels
-writer.add_vertex_color_uchar(r,g,b)
+writer.add_vertex_normal(vdata3[0,:], vdata3[1,:], vdata3[2,:])
+writer.add_vertex_rgba(r,g,b,a)
+writer.add_vertex_id()
+writer.add_vertex_piece(np.ones(num_vert))
 writer.add_vertex_channel("vdata1", "float", vdata1)
 writer.add_vertex_channel("vdata2", "uint", vdata2)
-writer.add_vertex_normal(vdata3[0,:], vdata3[1,:], vdata3[2,:])
+writer.add_face_id()
+writer.add_face_piece(2*np.ones(num_face))
 writer.add_face_channel("fdata", "float", fdata)
 # export binary ply file
 writer.export("example.ply")
